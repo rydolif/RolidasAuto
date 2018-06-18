dcbf1fa930919a2c41e3a9dea2137aec66661510
rydolif <rudolifrudolif@gmail.com>
2018-06-18 18:55:50 +0300

    hover ссилок +

--- gulpfile.js
+++ gulpfile.js
@@ -42,10 +42,10 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/jquery.validate.js',
-		'app/libs/jquery.mask.min.js',
-		'app/libs/jquery.popupoverlay.js',
-		'app/libs/swiper/swiper.min.js',
+		// 'app/libs/jquery.validate.js',
+		// 'app/libs/jquery.mask.min.js',
+		// 'app/libs/jquery.popupoverlay.js',
+		// 'app/libs/swiper/swiper.min.js',
 		// 'app/libs/fancybox/jquery.fancybox.js',
 		'app/js/common.js', // Always at the end
 		])
