67b2087d52719880bf99c9e04fc56e0e9a1b94cc
rydolif <rudolifrudolif@gmail.com>
2018-06-29 14:51:07 +0300

    сторінка калькулятора все окрцм адаптива

--- app/sendmail.php
+++ app/sendmail.php
@@ -1,15 +1,25 @@
 <?php
-	$SITE_TITLE = 'LeoLoft';
+	$SITE_TITLE = 'RolidasAuto';
 	$SITE_DESCR = '';
 
 	if ( isset($_POST) ) {
 		$name = htmlspecialchars(trim($_POST['name']));
 		$phone = htmlspecialchars(trim($_POST['phone']));
-		$number = htmlspecialchars(trim($_POST['number']));
-		$product = htmlspecialchars(trim($_POST['product']));
 		$subject = $_POST['subject'] ? htmlspecialchars(trim($_POST['subject'])) : '';
-		$comment = isset($_POST['comment']) ? htmlspecialchars(trim($_POST['comment'])) : '';
-		$question = isset($_POST['question']) ? htmlspecialchars(trim($_POST['question'])) : '';
+
+		$link = isset($_POST['link']) ? htmlspecialchars(trim($_POST['link'])) : '';
+		$type = isset($_POST['type']) ? htmlspecialchars(trim($_POST['type'])) : '';
+		$make = isset($_POST['make']) ? htmlspecialchars(trim($_POST['make'])) : '';
+		$year = isset($_POST['year']) ? htmlspecialchars(trim($_POST['year'])) : '';
+		$bodywork = isset($_POST['bodywork']) ? htmlspecialchars(trim($_POST['bodywork'])) : '';
+		$engine = isset($_POST['engine']) ? htmlspecialchars(trim($_POST['engine'])) : '';
+		$kw = isset($_POST['kw']) ? htmlspecialchars(trim($_POST['kw'])) : '';
+		$transmission = isset($_POST['transmission']) ? htmlspecialchars(trim($_POST['transmission'])) : '';
+		$amount = isset($_POST['amount']) ? htmlspecialchars(trim($_POST['amount'])) : '';
+		$mileage = isset($_POST['mileage']) ? htmlspecialchars(trim($_POST['mileage'])) : '';
+		$country = isset($_POST['country']) ? htmlspecialchars(trim($_POST['country'])) : '';
+		$vin = isset($_POST['vin']) ? htmlspecialchars(trim($_POST['vin'])) : '';
+
 		$to = 'rudolifrudolif@gmail.com';
 
 		$headers = "From: $SITE_TITLE \r\n";
@@ -21,21 +31,52 @@
 		$data .= 'Телефон: '.$phone."<br>";
 
 
-		if ( $number != '' ) {
-			$data .= 'Количество: ' . $number ."<br>";
+		if ( $link) {
+			$data .= 'Ссылка: ' . $link ."<br>";
 		}
 
-		if ( $product != '' ) {
-			$data .= 'Название товара: ' . $product ."<br>";
+		if ( $type) {
+			$data .= 'Тип автомобиля: ' . $type ."<br>";
 		}
 
-
-		if ( $comment != '' ) {
-			$data .= 'Комментарий: ' . $comment;
+		if ( $make) {
+			$data .= 'Марка: ' . $make ."<br>";
 		}
 
-		if ( $question != '' ) {
-			$data .= 'Вопрос: ' . $question;
+		if ( $year) {
+			$data .= 'Год выпуска: ' . $year ."<br>";
+		}
+
+		if ( $bodywork) {
+			$data .= 'Тип кузова: ' . $bodywork ."<br>";
+		}
+
+		if ( $engine) {
+			$data .= 'Тип двигателя: ' . $engine ."<br>";
+		}
+
+		if ( $kw) {
+			$data .= 'Количество KW: ' . $kw ."<br>";
+		}
+
+		if ( $transmission) {
+			$data .= 'Коробка передач: ' . $transmission ."<br>";
+		}
+
+		if ( $amount) {
+			$data .= 'Объем двигателя: ' . $amount ."<br>";
+		}
+
+		if ( $mileage) {
+			$data .= 'Пробег: ' . $mileage ."<br>";
+		}
+
+		if ( $country) {
+			$data .= 'Страна производитель: ' . $country ."<br>";
+		}
+
+		if ( $vin) {
+			$data .= 'VIN номер: ' . $vin ."<br>";
 		}
 
 		$message = "<div style='background:#ccc;border-radius:10px;padding:20px;'>
